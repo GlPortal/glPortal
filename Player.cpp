53c9ad637d3b42fe867a7f78dac7753ef215e55a
SimonLarsen <simonhffh@gmail.com>
2012-09-22 20:26:48 +0200

    Added buggy clamping to portal placement. Shot->Portal should be moved to Portal class.

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,5 @@
-#include <math.h>
-#include <stdio.h>
+#include <cmath>
+#include <cstdio>
 #include <GL/glut.h>
 #include "Player.hpp"
 
--- Shot.cpp
+++ Shot.cpp
@@ -1,4 +1,4 @@
-#include <math.h>
+#include <cmath>
 #include "Shot.hpp"
 
 #define RADDEG 57.29577951308232088 // 180/PI
@@ -73,10 +73,16 @@ void Shot::draw(GLuint *textures, float xrot, float yrot) {
  * @param box The box to place a portal on
  * @param portal The portal to place
  */
+
 void Shot::placePortal(Box &box, Portal &portal) {
 	float dist[4];
 	int min;
 
+	// Clamp y-coordinate
+	y = fmax(y, box.y1+1.0f);
+	y = fmin(y, box.y2-1.6f);
+
+	// Calculate distance from shot to planes
 	dist[0] = x - box.x1; // Distance from left face to x
 	dist[1] = box.x2 - x; // Distance from right face to x
 	dist[2] = z - box.z1; // Distance from back face to z
@@ -90,17 +96,35 @@ void Shot::placePortal(Box &box, Portal &portal) {
 		}
 	}
 
-	// Left face
-	if(min == 0) {
-		portal.place(box.x1,y,z, PD_LEFT);
-	// Right face
-	} else if(min == 1) {
-		portal.place(box.x2,y,z, PD_RIGHT);
-	// Back face
-	} else if(min == 2) {
-		portal.place(x,y,box.z1, PD_BACK);
-	// Front Face
+	// Portal on the YZ-plane
+	if(min <= 1) {
+		// Make sure box is wide enough
+		if(box.z2 - box.z1 < 2) return;
+		// Clamp z-coordinate
+		z = fmax(z, box.z1+1.1f);
+		z = fmin(z, box.z2-1.1f);
+
+		// Left face
+		if(min == 0) {
+			portal.place(box.x1,y,z, PD_LEFT);
+		// Right face
+		} else {
+			portal.place(box.x2,y,z, PD_RIGHT);
+		}
+
+	// Portal on the XY-plane
 	} else {
-		portal.place(x,y,box.z2, PD_FRONT);
+		// Make sure box is wide enough
+		if(box.x2 - box.x1 < 2.f) return;
+		// Clamp x-coordinate
+		x = fmax(x, box.x1+1.1f);
+		
+		// Back face
+		if(min == 2) {
+			portal.place(x,y,box.z1, PD_BACK);
+		// Front Face
+		} else {
+			portal.place(x,y,box.z2, PD_FRONT);
+		}	x = fmin(x, box.x2-1.1f);
 	}
 }
