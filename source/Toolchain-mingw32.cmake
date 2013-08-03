660c1068237996b5c61d4efc05c575e36eb46f59
Henry Hirsch <linux@wheep.de>
2013-08-03 19:29:14 +0200

    [feature] Map to wavefront converter skeleton.

--- source/Toolchain-mingw32.cmake
+++ source/Toolchain-mingw32.cmake
@@ -1,13 +1,16 @@
-  # the name of the target operating system
+ include(CMakeForceCompiler)
+ # the name of the target operating system
   SET(CMAKE_SYSTEM_NAME Windows)
 
   # which compilers to use for C and C++
-  SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-  SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
-  SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
+  SET(CMAKE_FORCE_C_COMPILER i686-w64-mingw32-gcc)
+  SET(CMAKE_FORCE_CXX_COMPILER i686-w64-mingw32-g++)
+  SET(CMAKE_FORCE_RC_COMPILER i686-w64-mingw32-windres)
+  SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
   # here is the target environment located
-  SET(CMAKE_FIND_ROOT_PATH  /usr/lib/gcc/i586-mingw32msvc)
+#  SET(CMAKE_FIND_ROOT_PATH /usr/lib/gcc/x86_64-w64-mingw32 /usr/i686-w64-mingw32/include /usr/i686-w64-mingw32/lib)
+  SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
 
   # adjust the default behaviour of the FIND_XXX() commands:
   # search headers and libraries in the target environment, search 
---
+++ source/map/MapToWavefront.cpp
@@ -0,0 +1,13 @@
+#include "MapToWavefront.hpp"
+#include <iostream>
+
+namespace glPortal {
+  namespace map {
+      const char* MapToWavefront::DATA_DIRECTORY = "data";
+
+      MapToWavefront::MapToWavefront(const string & filename){
+        cout << "Test";
+      }
+    
+  }
+}
