3081923d3201d7983ec1be934ec9af278983bf9f
Henry Hirsch <linux@wheep.de>
2013-08-17 11:38:31 +0200

    [feature] Map converter skeleton.

--- source/map/MapToWavefront.cpp
+++ source/map/MapToWavefront.cpp
@@ -1,12 +1,33 @@
 #include "MapToWavefront.hpp"
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 namespace glPortal {
   namespace map {
       const char* MapToWavefront::DATA_DIRECTORY = "data";
 
       MapToWavefront::MapToWavefront(const string & filename){
-        cout << "Test";
+	char * inputFilename  = new char [filename.length()+1];
+	char * resultFilename = new char [filename.length()+5];
+	strcpy (inputFilename, filename.c_str());
+
+	ifstream inputFileStream(inputFilename);
+	ofstream outputFileStream;
+	
+	strcpy(resultFilename, inputFilename);
+	strcat(resultFilename, ".obj");
+
+      	outputFileStream.open(resultFilename, ios::trunc);
+	string line;
+
+	while(getline(inputFileStream, line)) {
+	  cout << line << endl;
+	  outputFileStream << line << endl;
+	}
+ 
+	inputFileStream.close();
+	outputFileStream.close();
       }
     
   }
