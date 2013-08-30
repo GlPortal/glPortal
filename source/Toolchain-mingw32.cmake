b2f28d8788409963879d59ef9afb5af03badfe71
Henry Hirsch <linux@wheep.de>
2013-08-30 19:33:06 +0200

    [feature] Still failing win32 build support.

--- source/Toolchain-mingw32.cmake
+++ source/Toolchain-mingw32.cmake
@@ -1,21 +1,23 @@
- include(CMakeForceCompiler)
- # the name of the target operating system
+if(WIN)
+  include(CMakeForceCompiler)
   SET(CMAKE_SYSTEM_NAME Windows)
 
-  # which compilers to use for C and C++
   SET(CMAKE_FORCE_C_COMPILER i686-w64-mingw32-gcc)
   SET(CMAKE_FORCE_CXX_COMPILER i686-w64-mingw32-g++)
   SET(CMAKE_FORCE_RC_COMPILER i686-w64-mingw32-windres)
   SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
-  # here is the target environment located
-#  SET(CMAKE_FIND_ROOT_PATH /usr/lib/gcc/x86_64-w64-mingw32 /usr/i686-w64-mingw32/include /usr/i686-w64-mingw32/lib)
+  SET(CMAKE_CXX_FLAGS_RELEASE  "${CMAKE_CXX_FLAGS_RELEASE} -march=pentium4 -mtune=pentium4 -mwindows -O2")
+  SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-Wl,--no-undefined -static-libgcc -Wl,-O1 -Wl,--as-needed -Wl,--sort-common -s")
+
   SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
 
-  # adjust the default behaviour of the FIND_XXX() commands:
-  # search headers and libraries in the target environment, search 
-  # programs in the host environment
-  set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-  set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-  set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
+  SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+  SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+  SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+  include_directories(
+  /usr/i686-w64-mingw32/include
+  /usr/lib/gcc/i686-w64-mingw32/4.6/include 
+  /usr/i686-w64-mingw32/include
+  )
+endif(WIN)
