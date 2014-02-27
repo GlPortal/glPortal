44ab106e26a01df26b4ced40a7d3661c57b21995
Henry Hirsch <henry@w3-net.de>
2014-02-27 13:18:16 +0100

    [fix] Working toolchain.

--- source/Toolchain-mingw32.cmake
+++ source/Toolchain-mingw32.cmake
@@ -1,13 +1,9 @@
   include(CMakeForceCompiler)
   SET(CMAKE_SYSTEM_NAME Windows)
 
-  SET(CMAKE_FORCE_C_COMPILER i686-w64-mingw32-gcc)
-  SET(CMAKE_FORCE_CXX_COMPILER i686-w64-mingw32-g++)
-  SET(CMAKE_FORCE_RC_COMPILER i686-w64-mingw32-windres)
+  SET(CMAKE_C_COMPILER   i686-w64-mingw32-gcc)
+  SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
   SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
-
-  SET(CMAKE_CXX_FLAGS_RELEASE  "${CMAKE_CXX_FLAGS_RELEASE} -march=pentium4 -mtune=pentium4 -mwindows -O2")
-  SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-Wl,--no-undefined -static-libgcc -Wl,-O1 -Wl,--as-needed -Wl,--sort-common -s")
 
   SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
 
