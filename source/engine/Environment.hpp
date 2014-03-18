a858515b4831aaf49e935cd31f7096f1e58ac593
Henry Hirsch <henry@w3-net.de>
2014-03-18 09:10:13 +0100

    [feature] Environment stubs.

---
+++ source/engine/Environment.hpp
@@ -0,0 +1,25 @@
+#ifndef __ENVIRONMENT_HPP
+#define __ENVIRONMENT_HPP
+
+#include <vector>
+#include <string>
+#include <cstring>
+#include <cstdlib> 
+#include <map>
+#include "../util/ConfigFileParser.hpp"
+using namespace std;
+using namespace glPortal::util;
+
+namespace glPortal {
+  namespace engine{
+    class Environment{
+    private:
+      ConfigFileParser config;
+    public:
+      void init();
+      ConfigFileParser getConfig();
+    };
+  }
+}
+
+#endif
