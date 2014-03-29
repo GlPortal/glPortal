c254621129881888501a72620a64583ba53bfd7e
Julian Thijssen <Nimthora@gmail.com>
2014-03-29 21:48:01 +0100

    Portals use Vector3f's

--- source/Portal.hpp
+++ source/Portal.hpp
@@ -2,10 +2,12 @@
 #define __PORTAL_HPP
 
 #include "engine/Box.hpp"
+#include "util/Vector3f.hpp"
 class Map; // Forward declaration
 #include "GameMap.hpp"
 
 using namespace glPortal::engine;
+using namespace util;
 
 enum PORTAL_DIR { PD_RIGHT, PD_FRONT, PD_LEFT, PD_BACK, PD_NONE }; /**< Possible direction a portal can face */
 enum PORTAL_COLOR { PC_BLUE, PC_ORANGE }; /**< Colors a portal can have */
@@ -15,7 +17,7 @@ enum PORTAL_COLOR { PC_BLUE, PC_ORANGE }; /**< Colors a portal can have */
  */
 class Portal {
 public:
-	Portal() : x(0), y(0), z(0), dir(PD_NONE), active(false) { }
+	Portal() : dir(PD_NONE), active(false) { }
 
         void place(float x, float y, float z, PORTAL_DIR dir, GameMap& gameMap);
         void placeOnBox(Box &box, float hitx, float hity, float hitz, GameMap& gameMap);
@@ -27,7 +29,7 @@ public:
 	void drawStencil();
 	void drawOutline(PORTAL_COLOR color);
 
-	float x,y,z;
+	Vector3f position;
 	PORTAL_DIR dir; /**< Direction portal is facing */
 	bool active;	/**< True if the portal is currently active */
 };
