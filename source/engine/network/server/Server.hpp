8d619604e914c3fc84f9e0b77b05f620b1cd094f
Henry Hirsch <henry@w3-net.de>
2014-06-01 15:27:40 +0200

    [feature] Removed nested namespaces.

--- source/engine/network/server/Server.hpp
+++ source/engine/network/server/Server.hpp
@@ -11,22 +11,18 @@
 using boost::asio::ip::udp;
 
 namespace glPortal {
-  namespace network {
-    namespace server {
-      class Server {
-      public:
-        Server(boost::asio::io_service& io_service, short port);
-        void getReceiveHandle(const boost::system::error_code& error,size_t bytes_recvd);
-        void getSendHandle(const boost::system::error_code& /*error*/,  size_t /*bytes_sent*/);
-      private:
-        boost::asio::io_service& io_service;
-        udp::socket socket;
-        udp::endpoint sender_endpoint;
-        enum { max_length = 1024 };
-        char data[max_length];
-        std::vector<std::string> log;
-      };
-    }
-  }
+  class Server {
+  public:
+    Server(boost::asio::io_service& io_service, short port);
+    void getReceiveHandle(const boost::system::error_code& error,size_t bytes_recvd);
+    void getSendHandle(const boost::system::error_code& /*error*/,  size_t /*bytes_sent*/);
+  private:
+    boost::asio::io_service& io_service;
+    udp::socket socket;
+    udp::endpoint sender_endpoint;
+    enum { max_length = 1024 };
+    char data[max_length];
+    std::vector<std::string> log;
+  };
 }
 #endif
