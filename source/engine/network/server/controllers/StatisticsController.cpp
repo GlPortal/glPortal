fd9e6a83980d2e4793779338dd9c836c18097c0c
Henry Hirsch <henry@w3-net.de>
2014-06-02 15:28:31 +0200

    [feature] Create statistics in intervals.

--- source/engine/network/server/controllers/StatisticsController.cpp
+++ source/engine/network/server/controllers/StatisticsController.cpp
@@ -1,8 +1,15 @@
 #include "StatisticsController.hpp"
 #include <fstream>
 #include <boost/filesystem.hpp>
+#include "../../../../util/sheduler/Sheduler.hpp"
 
-namespace glPortal {     
+namespace glPortal {
+  StatisticsController::StatisticsController(){
+    sheduler = new Sheduler(1000);
+    sheduler->add(this);
+    sheduler->start();
+  }
+  
   std::string StatisticsController::setMessage(std::string message){
           
       log.push_back(message);
@@ -10,22 +17,25 @@ namespace glPortal {
       if(log.size() > 10){
         log.erase(log.begin());
       }
-      std::ofstream myfile;
-      const char dir_path[] = "html";
+    return std::string("message The statistics controller logged your message\n");
+  }
 
-      boost::filesystem::path dir(dir_path);
-      if(boost::filesystem::create_directory(dir)) {
-        std::cout << "HTML directory created." << "\n";
-      }
-      myfile.open ("html/index.html");
+  void StatisticsController::run(){
+    std::ofstream myfile;
+    const char dir_path[] = "html";
 
-      std::cout << "LOG:";
-      for (std::vector<std::string>::iterator it = log.begin() ; it != log.end(); ++it){
-        std::cout << "entry:";
+    boost::filesystem::path dir(dir_path);
+    if(boost::filesystem::create_directory(dir)) {
+      std::cout << "HTML directory created." << "\n";
+    }
+    myfile.open ("html/index.html");
+    
+    std::cout << "LOG:\n";
+    for (std::vector<std::string>::iterator it = log.begin() ; it != log.end(); ++it){
+        std::cout << "entry:\n";
         myfile << *it;
-      }
-      myfile.close();
-
-    return std::string("statistics controller\n");
+    }
+    myfile.close();
+      
   }
 }
--- source/engine/network/server/controllers/StatisticsController.hpp
+++ source/engine/network/server/controllers/StatisticsController.hpp
@@ -3,13 +3,19 @@
 
 #include <string>
 #include <vector>
+#include "../../../../util/sheduler/Sheduler.hpp"
+#include "../../../../util/sheduler/Job.hpp"
+#include "../ControllerInterface.hpp"
 
 namespace glPortal {
-  class StatisticsController {
+  class StatisticsController : public ControllerInterface, public Job{
   public:
+    StatisticsController();
     std::string setMessage(std::string message);
+    void run();
   private:
     std::vector<std::string> log;
+    Sheduler *sheduler;
   };
 }
 #endif
