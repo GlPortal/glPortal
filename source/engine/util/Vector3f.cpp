a3bf7e10bd0b1912bc7d81deda89aad4903e1f8e
Henry Hirsch <henry@w3-net.de>
2014-06-30 21:46:16 +0200

    [fix] Initialized variables in Vector3f.

--- source/engine/util/Vector3f.cpp
+++ source/engine/util/Vector3f.cpp
@@ -5,10 +5,8 @@
 
 namespace glPortal {
 
-Vector3f::Vector3f(float x, float y, float z) {
-  this->x = x;
-  this->y = y;
-  this->z = z;
+Vector3f::Vector3f(float x, float y, float z): x(0), y(0), z(0) {
+  this->set(x, y, z);
 }
 
 void Vector3f::set(float x, float y, float z) {
