078cc5edd91b99f46148c1f978e894096f52f17e
Henry Hirsch <henry@w3-net.de>
2014-07-27 11:54:45 +0200

    [feature] Avoided redeclaring the same constant multiple times.

---
+++ source/engine/util/Math.cpp
@@ -0,0 +1,5 @@
+#include "Math.hpp"
+
+namespace glPortal {
+  const float Math::PI_RND=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089;
+} /* namespace glPortal */
---
+++ source/engine/util/Math.hpp
@@ -0,0 +1,13 @@
+#ifndef MATH_HPP
+#define MATH_HPP
+
+namespace glPortal {
+
+class Math {
+public:
+  static const float PI_RND;
+};
+
+} /* namespace glPortal */
+
+#endif
