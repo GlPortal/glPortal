27869390b375788934061954715c7c34daf936d9
Henry Hirsch <henry@w3-net.de>
2014-07-27 12:41:24 +0200

    [refactoring] Calling RAD_TO_DEG by name and removing duplicated code.

--- source/engine/util/Math.hpp
+++ source/engine/util/Math.hpp
@@ -6,6 +6,8 @@ namespace glPortal {
 class Math {
 public:
   static const float PI_RND;
+  static float RAD_TO_DEG(float radius);
+  static float DEG_TO_RAD(float degree);
 };
 
 } /* namespace glPortal */
