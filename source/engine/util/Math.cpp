589a99246e5ca85f1814b10b2014a31d39927c9f
Nimthora <julian.thijssen@student.uva.nl>
2014-08-08 05:23:48 +0200

    Cleaned up the Math class to follow the standard

--- source/engine/util/Math.cpp
+++ source/engine/util/Math.cpp
@@ -3,11 +3,12 @@
 namespace glPortal {
   const float Math::PI_RND=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089;
   
-  float Math::RAD_TO_DEG(float radius){
-    return radius * (180 / Math::PI_RND);
+  float Math::toDegrees(float radians) {
+    return radians * (180 / PI_RND);
   }
 
-  float Math::DEG_TO_RAD(float degree){
-    return degree * (Math::PI_RND / 180);
+  float Math::toRadians(float degrees) {
+    return degrees * (PI_RND / 180);
   }
+
 } /* namespace glPortal */
