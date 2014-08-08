fc63865fd2dd6b4ccadfb83a9e8801199de0f06a
Nimthora <julian.thijssen@student.uva.nl>
2014-08-08 20:46:51 +0200

    Intermediate test build

--- source/engine/Camera.cpp
+++ source/engine/Camera.cpp
@@ -16,13 +16,24 @@ Camera::Camera(float fovy, float aspect, float zNear, float zFar) {
 
 void Camera::recalculate() {
   aspect = Window::aspect;
-  float fovyr = fovy * (Math::PI_RND / 180);
-  projectionMatrix.array[0] = (float) (1 / tan(fovyr / 2)) / aspect;
-  projectionMatrix.array[5] = (float) (1 / tan(fovyr / 2));
-  projectionMatrix.array[10] = (zNear + zFar) / (zNear - zFar);
-  projectionMatrix.array[11] = -1;
-  projectionMatrix.array[14] = (2 * zNear * zFar) / (zNear - zFar);
-  projectionMatrix.array[15] = 0;
+
+  projectionMatrix.setIdentity();
+  if(perspective) {
+    float fovyr = Math::toRadians(fovy);
+    projectionMatrix.array[0] = (float) (1 / tan(fovyr / 2)) / aspect;
+    projectionMatrix.array[5] = (float) (1 / tan(fovyr / 2));
+    projectionMatrix.array[10] = (zNear + zFar) / (zNear - zFar);
+    projectionMatrix.array[11] = -1;
+    projectionMatrix.array[14] = (2 * zNear * zFar) / (zNear - zFar);
+    projectionMatrix.array[15] = 0;
+  } else {
+    projectionMatrix.array[0] = 2 / (right - left);
+    projectionMatrix.array[5] = 2 / (top - bottom);
+    projectionMatrix.array[10] = -2 / (zFar - zNear);
+    projectionMatrix.array[12] = (left - right) / (right - left);
+    projectionMatrix.array[13] = (bottom - top) / (top - bottom);
+    projectionMatrix.array[14] = (zNear - zFar) / (zFar - zNear);
+  }
 }
 
 Matrix4f Camera::getProjectionMatrix() {
@@ -46,6 +57,16 @@ void Camera::setZNear(float zNear) {
 
 void Camera::setZFar(float zFar) {
   this->zFar = zFar;
+  recalculate();
+}
+
+void Camera::setPerspective() {
+  this->perspective = true;
+  recalculate();
+}
+
+void Camera::setOrthographic() {
+  this->perspective = false;
   recalculate();
 }
 
--- source/engine/Camera.hpp
+++ source/engine/Camera.hpp
@@ -8,13 +8,26 @@ const float DEFAULT_FOVY = 60;
 const float DEFAULT_ASPECT = 1;
 const float DEFAULT_ZNEAR = 0.1f;
 const float DEFAULT_ZFAR = 100;
+const bool  DEFAULT_PERSPECTIVE = true;
+const float DEFAULT_TOP = 1;
+const float DEFAULT_BOTTOM = -1;
+const float DEFAULT_LEFT = -1;
+const float DEFAULT_RIGHT = 1;
 
 namespace glPortal {
 
 class Camera {
 public:
   Camera() :
-      fovy(DEFAULT_FOVY), aspect(DEFAULT_ASPECT), zNear(DEFAULT_ZNEAR), zFar(DEFAULT_ZFAR) {
+      fovy(DEFAULT_FOVY),
+      aspect(DEFAULT_ASPECT),
+      zNear(DEFAULT_ZNEAR),
+      zFar(DEFAULT_ZFAR),
+      perspective(DEFAULT_PERSPECTIVE),
+      top(DEFAULT_TOP),
+      bottom(DEFAULT_BOTTOM),
+      left(DEFAULT_LEFT),
+      right(DEFAULT_RIGHT) {
     recalculate();
   }
   Camera(float fovy, float aspect, float zNear, float zFar);
@@ -24,6 +37,8 @@ public:
   void setAspect(float aspect);
   void setZNear(float zNear);
   void setZFar(float zFar);
+  void setPerspective();
+  void setOrthographic();
 
   Vector3f position;
   Vector3f rotation;
@@ -33,6 +48,11 @@ private:
   float aspect;
   float zNear;
   float zFar;
+  bool perspective;
+  float top;
+  float bottom;
+  float left;
+  float right;
 };
 
 } /* namespace glPortal */
