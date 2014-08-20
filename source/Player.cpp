278cd327f765d32e1fa7267393c3a9226059180e
Henry Hirsch <henry@w3-net.de>
2014-08-20 21:02:36 +0200

    [feature] Added health to player and methods to increase and deacrease health and check if the player is alive.

--- source/Player.cpp
+++ source/Player.cpp
@@ -1,5 +1,18 @@
 #include "Player.hpp"
 
 namespace glPortal {
-
+  void Player::harm(int amount){
+    health = health - amount;
+    if(health < MIN_HEALTH) health = MIN_HEALTH;
+  }
+  
+  void Player::heal(int amount){
+    health = health + amount;
+    if(health > MAX_HEALTH) health = MAX_HEALTH;
+  }
+  
+  bool Player::isAlive(){
+    if(health >= 0) return true;
+    return false;
+  }
 } /* namespace glPortal */
--- source/Player.hpp
+++ source/Player.hpp
@@ -13,6 +13,13 @@ public:
     scale.set(0.5f, 1, 0.5f);
   }
   Vector3f velocity;
+private:
+  const int MIN_HEALTH = 100;
+  const int MAX_HEALTH = -100;
+  int health;
+  void harm(int amount);
+  void heal(int amount);
+  bool isAlive();
 };
 
 } /* namespace glPortal */
