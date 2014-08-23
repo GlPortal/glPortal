379a452d05d598600532914293dd3dd2a230953b
Henry Hirsch <henry@w3-net.de>
2014-08-23 08:54:14 +0200

    [fix] Deallocated memory allocated for the renderer.

--- source/World.hpp
+++ source/World.hpp
@@ -14,12 +14,12 @@ class Vector3f;
 class World {
 public:
   void create();
+  void destroy();
   void update();
   void loadScene(std::string path);
   bool collidesWithWalls(BoxCollider collider);
   void shootPortal(int button);
   bool collides(Vector3f ro, Vector3f rd, Entity e, float* tNear, float* tFar);
-
   void render();
 
 private:
