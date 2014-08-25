70a8e02c8bcc2c283f4e0e062f2da0bbe2e407aa
Nimthora <julian.thijssen@student.uva.nl>
2014-08-25 20:17:27 +0200

    Cleaned up some portal shooting code

--- source/Portal.cpp
+++ source/Portal.cpp
@@ -1,162 +1,215 @@
 #include <engine/BoxCollider.hpp>
+#include <engine/loader/MeshLoader.hpp>
+#include <engine/Mesh.hpp>
 #include <Portal.hpp>
-#include <cstdio>
+#include <cmath>
+//#include <cstdio>
 
 namespace glPortal {
-  const int Portal::PORTAL_RANGE = 1000;
-  const Vector3f Portal::BLUE_COLOR = Vector3f(0.33, 0.57, 1);
-  const Vector3f Portal::ORANGE_COLOR = Vector3f(1, 0.76, 0.33);
 
-  Vector3f Portal::getDirection() {
-    if(rotation.x == 0) {
-      if(rotation.y == 90) { return Vector3f(-1, 0, 0); }
-      if(rotation.y == -90) { return Vector3f(1, 0, 0); }
-      if(rotation.y == 0) { return Vector3f(0, 0, -1); }
-      if(rotation.y == 180) { return Vector3f(0, 0, 1); }
+const int Portal::PORTAL_RANGE = 1000;
+const Vector3f Portal::BLUE_COLOR = Vector3f(0.33, 0.57, 1);
+const Vector3f Portal::ORANGE_COLOR = Vector3f(1, 0.76, 0.33);
+
+Vector3f Portal::getDirection() {
+  return direction;
+}
+
+bool Portal::throughPortal(BoxCollider collider) {
+  if(rotation.x == 0) {
+    if(rotation.y == 90) {
+      if(collider.position.x > position.x &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
+        return true;
+      }
     }
+    if(rotation.y == -90) {
+      if(collider.position.x < position.x &&
+         collider.position.x - collider.size.x/2 > position.x - scale.x &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
+        return true;
+      }
+    }
+    if(rotation.y == 0) {
+      if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
+         collider.position.z > position.z &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z) {
+        return true;
+      }
+    }
+    if(rotation.y == 180) {
+      if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
+         collider.position.z < position.z &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z) {
+        return true;
+      }
+    }
+  } else {
     if(rotation.x == -90) {
-      return Vector3f(0, -1, 0);
+      if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
+         collider.position.y > position.y &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
+        return true;
+      }
     }
     if(rotation.x == 90) {
-      return Vector3f(0, 1, 0);
+      if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
+         collider.position.y < position.y &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
+        return true;
+      }
     }
-    return Vector3f(0, 0, -1);
   }
+  return false;
+}
 
-  bool Portal::throughPortal(BoxCollider collider) {
-    if(rotation.x == 0) {
-      if(rotation.y == 90) {
-        if(collider.position.x > position.x &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
-          return true;
-        }
-      }
-      if(rotation.y == -90) {
-        if(collider.position.x < position.x &&
-           collider.position.x - collider.size.x/2 > position.x - scale.x &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
-          return true;
-        }
-      }
-      if(rotation.y == 0) {
-        if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
-           collider.position.z > position.z &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z) {
-          return true;
-        }
-      }
-      if(rotation.y == 180) {
-        if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
-           collider.position.z < position.z &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z) {
-          return true;
-        }
-      }
-    } else {
-      if(rotation.x == -90) {
-        if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
-           collider.position.y > position.y &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
-          return true;
-        }
-      }
-      if(rotation.x == 90) {
-        if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
-           collider.position.y < position.y &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
-          return true;
-        }
-      }
-    }
+bool Portal::inPortal(BoxCollider collider) {
+  if(!open) {
     return false;
   }
-
-  bool Portal::inPortal(BoxCollider collider) {
-    if(!open) {
-      return false;
-    }
-    if(rotation.x == 0) {
-      if(rotation.y == 90) {
-        if(collider.position.x + collider.size.x/2 > position.x &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
-          return true;
-        }
-      }
-      if(rotation.y == -90) {
-        if(collider.position.x - collider.size.x/2  < position.x &&
-           collider.position.x - collider.size.x/2 > position.x - scale.x &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
-          return true;
-        }
-      }
-      if(rotation.y == 0) {
-        if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
-           collider.position.z + collider.size.z/2 > position.z &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z) {
-          return true;
-        }
-      }
-      if(rotation.y == 180) {
-        if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
-           collider.position.z - collider.size.z/2 < position.z &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z) {
-          return true;
-        }
-      }
-    } else {
-      if(rotation.x == -90) {
-        if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
-           collider.position.y + collider.size.y/2 > position.y &&
-           collider.position.y + collider.size.y/2 < position.y + scale.y &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
-          return true;
-        }
-      }
-      if(rotation.x == 90) {
-        if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
-           collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
-           collider.position.y - collider.size.y/2 < position.y &&
-           collider.position.y - collider.size.y/2 > position.y - scale.y &&
-           collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
-           collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
-          return true;
-        }
+  if(rotation.x == 0) {
+    if(rotation.y == 90) {
+      if(collider.position.x + collider.size.x/2 > position.x &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
+        return true;
       }
     }
-    return false;
+    if(rotation.y == -90) {
+      if(collider.position.x - collider.size.x/2  < position.x &&
+         collider.position.x - collider.size.x/2 > position.x - scale.x &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
+        return true;
+      }
+    }
+    if(rotation.y == 0) {
+      if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
+         collider.position.z + collider.size.z/2 > position.z &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z) {
+        return true;
+      }
+    }
+    if(rotation.y == 180) {
+      if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y/2 &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y/2 &&
+         collider.position.z - collider.size.z/2 < position.z &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z) {
+        return true;
+      }
+    }
+  } else {
+    if(rotation.x == -90) {
+      if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
+         collider.position.y + collider.size.y/2 > position.y &&
+         collider.position.y + collider.size.y/2 < position.y + scale.y &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
+        return true;
+      }
+    }
+    if(rotation.x == 90) {
+      if(collider.position.x - collider.size.x/2 > position.x - scale.x/2 &&
+         collider.position.x + collider.size.x/2 < position.x + scale.x/2 &&
+         collider.position.y - collider.size.y/2 < position.y &&
+         collider.position.y - collider.size.y/2 > position.y - scale.y &&
+         collider.position.z - collider.size.z/2 > position.z - scale.z/2 &&
+         collider.position.z + collider.size.z/2 < position.z + scale.z/2) {
+        return true;
+      }
+    }
   }
+  return false;
+}
+
+void Portal::placeOnWall(BoxCollider wall, Vector3f point) {
+  //Determine on what side the portal is
+  //Side 0: -x, Side 1: x, Side 2: -z, Side 3: z, Side 4: -y, Side 5: y
+  float dist = 1000000;
+  int side = 0;
+  float distances[6] = {(float) fabs(point.x - (wall.position.x - wall.size.x / 2)),
+                        (float) fabs(point.x - (wall.position.x + wall.size.x / 2)),
+                        (float) fabs(point.z - (wall.position.z - wall.size.z / 2)),
+                        (float) fabs(point.z - (wall.position.z + wall.size.z / 2)),
+                        (float) fabs(point.y - (wall.position.y - wall.size.y / 2)),
+                        (float) fabs(point.y - (wall.position.y + wall.size.y / 2))};
+
+  for (int i = 0; i < 6; i++) {
+    if (distances[i] < dist) {
+      side = i;
+      dist = distances[i];
+    }
+  }
+
+  position.set(point);
+
+  switch(side) {
+  case 0: direction.set(-1, 0, 0); break;
+  case 1: direction.set(1, 0, 0); break;
+  case 2: direction.set(0, 0, -1); break;
+  case 3: direction.set(0, 0, 1); break;
+  case 4: direction.set(0, -1, 0); break;
+  case 5: direction.set(0, 1, 0); break;
+  }
+
+  if (side == 0) {
+    rotation.y = 90;
+    scale.set(1, 2, 1);
+  }
+  if (side == 1) {
+    rotation.y = -90;
+    scale.set(1, 2, 1);
+  }
+  if (side == 2) {
+    rotation.y = 0;
+    scale.set(1, 2, 1);
+  }
+  if (side == 3) {
+    rotation.y = 180;
+    scale.set(1, 2, 1);
+  }
+  if (side == 4) {
+    rotation.x = -90;
+    scale.set(1, 2, 2);
+  }
+  if (side == 5) {
+    rotation.x = 90;
+    scale.set(1, 2, 2);
+  }
+
+  position.add(*getDirection().scale(0.01f));
+  open = true;
+  mesh = MeshLoader::getMesh("Plane.obj");
+}
+
 } /* namespace glPortal */
