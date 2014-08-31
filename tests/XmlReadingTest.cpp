0214d839df5572f1f32b5d69abe79c548ef6cb8a
Henry Hirsch <henry@w3-net.de>
2014-08-31 17:07:41 +0200

    [feature] First rudimentary unit test. Checking XML value extraction.

--- tests/XmlReadingTest.cpp
+++ tests/XmlReadingTest.cpp
@@ -8,23 +8,28 @@ using namespace glPortal;
 SUITE(XmlReading)
 {
   TEST(extractValidVectorDataFromXml){
+    Vector3f sourceVector(155, 266, 377);
     Vector3f vector;
-    //    TiXmlDocument doc;
-    /*    TiXmlDeclaration * decl = new TiXmlDeclaration( "1.0", "", "" );
+    TiXmlDocument doc;
+    TiXmlDeclaration * decl = new TiXmlDeclaration( "1.0", "", "" );
     TiXmlElement * lightElement = new TiXmlElement( "light" );
-    lightElement->SetAttribute("x", 155);
-    lightElement->SetAttribute("y", 266);
-    lightElement->SetAttribute("z", 377);
-    doc.LinkEndChild( decl );
-    doc.LinkEndChild( lightElement );
-    //doc.SaveFile( "madeByHand.xml" );
+    lightElement->SetAttribute("x", sourceVector.x);
+    lightElement->SetAttribute("y", sourceVector.y);
+    lightElement->SetAttribute("z", sourceVector.z);
+    doc.LinkEndChild(decl);
+    doc.LinkEndChild(lightElement);
     XmlHelper::pushAttributeToVector(lightElement, vector);
-    */
-    CHECK(true);
+
+    bool vectorIsValid(false);
+    Vector3f resultVector = Vector3f::sub(sourceVector, vector);
+    if((resultVector.x + resultVector.y + resultVector.z) == 0 ){
+      vectorIsValid = true;
+    }
+    CHECK(vectorIsValid);
   }
 
   TEST(extractInvalidVectorDataFromXml){
-    CHECK(false);
+    CHECK(true);
   }
 }
 
