a54b2671efc47b60600388fbbbbcba8767cef35b
Henry Hirsch <henry@w3-net.de>
2014-08-31 21:26:12 +0200

    [feature] Test checking for exception in XmlHelper.

--- source/engine/loader/XmlHelper.cpp
+++ source/engine/loader/XmlHelper.cpp
@@ -12,15 +12,26 @@ using namespace std;
 
 namespace glPortal {
 
+  std::string XmlHelper::mandatoryAttributeMessage("Mandatory attribute has not been defined.");
 /*
  * Pushes vector coordinates from an element to a Vector3f
  */
   void XmlHelper::pushAttributeToVector(TiXmlElement* xmlElement, Vector3f &targetVector) {
 
     if(xmlElement) {
-      xmlElement->QueryFloatAttribute("x", &targetVector.x);
-      xmlElement->QueryFloatAttribute("y", &targetVector.y);
-      xmlElement->QueryFloatAttribute("z", &targetVector.z);
+      int xQueryResult = xmlElement->QueryFloatAttribute("x", &targetVector.x);
+      int yQueryResult = xmlElement->QueryFloatAttribute("y", &targetVector.y);
+      int zQueryResult = xmlElement->QueryFloatAttribute("z", &targetVector.z);
+      if(xQueryResult == TIXML_NO_ATTRIBUTE){
+        throw runtime_error(XmlHelper::mandatoryAttributeMessage + " <x>");
+      }
+      if(yQueryResult == TIXML_NO_ATTRIBUTE){
+        throw runtime_error(XmlHelper::mandatoryAttributeMessage + " <y>");
+      }
+      if(zQueryResult == TIXML_NO_ATTRIBUTE){
+        throw runtime_error(XmlHelper::mandatoryAttributeMessage + " <z>");
+      }
+
     }
   }  
 } /* namespace glPortal */
