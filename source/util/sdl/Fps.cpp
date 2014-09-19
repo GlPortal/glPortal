cab25374e7263c9711ff2dd7880547489629b9e2
Henry Hirsch <henry@w3-net.de>
2014-09-19 21:43:23 +0200

    [feature] Moved FPS counting from Game class to a util class.

---
+++ source/util/sdl/Fps.cpp
@@ -0,0 +1,37 @@
+#include "Fps.hpp"
+
+#include <SDL2/SDL_timer.h>
+#include <string>
+#include <cstdio>
+#include <iostream>
+
+namespace glPortal {
+
+Fps::Fps() {
+  fps = 0;
+  lastFpsTime = SDL_GetTicks();
+  rendered = 0;
+}
+
+void Fps::countCycle(){
+  rendered++;
+}
+
+/**
+  * Dump current FPS to stdout
+  */
+void Fps::dump(){
+  std::cout << "FPS: " << getFps() << std::endl;
+}
+  
+  
+int Fps::getFps() {
+    if (SDL_GetTicks() - lastFpsTime > 1000) {
+      lastFpsTime = SDL_GetTicks();
+      fps = rendered;
+      rendered = 0;
+    }
+    return fps;
+}
+
+} /* namespace glPortal */
