ceb3055d015d2adc9de865125b7acea8fe539b4e
Henry Hirsch <henry@w3-net.de>
2014-09-19 21:44:19 +0200

    [fix] Removed trailing whitespace.

--- source/util/sdl/Fps.cpp
+++ source/util/sdl/Fps.cpp
@@ -22,8 +22,7 @@ void Fps::countCycle(){
   */
 void Fps::dump(){
   std::cout << "FPS: " << getFps() << std::endl;
-}
-  
+}  
   
 int Fps::getFps() {
     if (SDL_GetTicks() - lastFpsTime > 1000) {
