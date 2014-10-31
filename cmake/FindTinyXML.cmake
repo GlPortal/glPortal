7d5caea97705ef064893275cb1b40d5f4d9633bb
xantares <xantares09@hotmail.com>
2014-10-31 09:05:56 +0000

    TinyXML platform check

---
+++ cmake/FindTinyXML.cmake
@@ -0,0 +1,28 @@
+# - Find TinyXML
+# Find the native TinyXML includes and library
+#
+#   TINYXML_FOUND       - True if TinyXML found.
+#   TINYXML_INCLUDE_DIR - where to find tinyxml.h, etc.
+#   TINYXML_LIBRARIES   - List of libraries when using TinyXML.
+#
+
+IF( TINYXML_INCLUDE_DIR )
+    # Already in cache, be silent
+    SET( TinyXML_FIND_QUIETLY TRUE )
+ENDIF( TINYXML_INCLUDE_DIR )
+
+FIND_PATH( TINYXML_INCLUDE_DIR "tinyxml.h"
+           PATH_SUFFIXES "tinyxml" )
+set (TINYXML_INCLUDE_DIRS ${TINYXML_INCLUDE_DIR})
+
+FIND_LIBRARY( TINYXML_LIBRARY
+              NAMES "tinyxml"
+              PATH_SUFFIXES "tinyxml" )
+set (TINYXML_LIBRARIES ${TINYXML_LIBRARY})
+
+# handle the QUIETLY and REQUIRED arguments and set TINYXML_FOUND to TRUE if
+# all listed variables are TRUE
+INCLUDE( "FindPackageHandleStandardArgs" )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( "TinyXML" DEFAULT_MSG TINYXML_LIBRARY TINYXML_INCLUDE_DIR )
+
+MARK_AS_ADVANCED( TINYXML_LIBRARY TINYXML_INCLUDE_DIR TINYXML_INCLUDE_DIRS TINYXML_LIBRARIES )
