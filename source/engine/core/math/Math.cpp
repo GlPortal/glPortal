39ae9bca40d7add4b6ec5b54515361b8f72251d6
Wouters Dorian <gravgun@openmailbox.org>
2015-05-25 18:03:18 +0200

    Optimize Math class

--- source/engine/core/math/Math.cpp
+++ source/engine/core/math/Math.cpp
@@ -4,44 +4,34 @@
 #include <cmath>
 
 namespace glPortal {
-  const float Math::PI_RND=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089;
-  
-  float Math::toDegrees(float radians) {
-    return radians * (180 / PI_RND);
+Vector3f Math::toDirection(const Vector3f &v) {
+  Matrix4f m;
+  m.rotate(v);
+
+  Vector3f direction = m.transform(Vector3f(0, 0, -1));
+  return direction;
+}
+
+Vector3f Math::toEuler(const Vector3f &direction) {
+  Vector3f euler;
+
+  //Pitch
+  euler.x = Math::toDegrees(asin(direction.y));
+
+  //Yaw
+  if(direction.x <= 0 && direction.z < 0) {
+    euler.y = Math::toDegrees(atan(fabs(direction.x) / fabs(direction.z)));
+  }
+  if(direction.x < 0 && direction.z >= 0) {
+    euler.y = Math::toDegrees(atan(fabs(direction.z) / fabs(direction.x))) + 90;
+  }
+  if(direction.x >= 0 && direction.z > 0) {
+    euler.y = Math::toDegrees(atan(fabs(direction.x) / fabs(direction.z))) + 180;
+  }
+  if(direction.x > 0 && direction.z <= 0) {
+    euler.y = Math::toDegrees(atan(fabs(direction.z) / fabs(direction.x))) + 270;
   }
 
-  float Math::toRadians(float degrees) {
-    return degrees * (PI_RND / 180);
-  }
-
-  Vector3f Math::toDirection(Vector3f v) {
-    Matrix4f m;
-    m.rotate(v);
-
-    Vector3f direction = m.transform(Vector3f(0, 0, -1));
-    return direction;
-  }
-
-  Vector3f Math::toEuler(Vector3f direction) {
-    Vector3f euler;
-
-    //Pitch
-    euler.x = Math::toDegrees(asin(direction.y));
-
-    //Yaw
-    if(direction.x <= 0 && direction.z < 0) {
-      euler.y = Math::toDegrees(atan(fabs(direction.x) / fabs(direction.z)));
-    }
-    if(direction.x < 0 && direction.z >= 0) {
-      euler.y = Math::toDegrees(atan(fabs(direction.z) / fabs(direction.x))) + 90;
-    }
-    if(direction.x >= 0 && direction.z > 0) {
-      euler.y = Math::toDegrees(atan(fabs(direction.x) / fabs(direction.z))) + 180;
-    }
-    if(direction.x > 0 && direction.z <= 0) {
-      euler.y = Math::toDegrees(atan(fabs(direction.z) / fabs(direction.x))) + 270;
-    }
-
-    return euler;
-  }
+  return euler;
+}
 } /* namespace glPortal */
