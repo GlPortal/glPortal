c46673988f0297f33f916cd3b4b66da7ac7d410d
Wouters Dorian <gravgun@openmailbox.org>
2015-05-25 17:33:11 +0200

    Optimize MeshLoader

--- source/assets/model/MeshLoader.hpp
+++ source/assets/model/MeshLoader.hpp
@@ -13,11 +13,11 @@ class Entity;
 
 class MeshLoader {
 public:
-  static Mesh getMesh(std::string path);
-  static Mesh getPortalBox(Entity wall);
+  static Mesh& getMesh(const std::string &path);
+  static Mesh getPortalBox(const Entity awall);
   static Mesh getSubPlane(int x, int y, int width, int height, int w, int h);
 private:
-  static Mesh uploadMesh(const aiMesh* mesh);
+  static Mesh uploadMesh(const aiMesh *mesh);
   static std::map<std::string, Mesh> meshCache;
 };
 
