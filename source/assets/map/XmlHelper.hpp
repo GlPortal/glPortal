7e9f4b3ec020faf724f80f519e6e4f30566e368f
Wouters Dorian <gravgun@openmailbox.org>
2015-05-25 17:25:57 +0200

    Style fixes to XmlHelper

--- source/assets/map/XmlHelper.hpp
+++ source/assets/map/XmlHelper.hpp
@@ -9,11 +9,11 @@ namespace glPortal {
 
 class XmlHelper {
 public:
-  static void pushAttributeVertexToVector(TiXmlElement* xmlElement, Vector3f &targetVector);
+  static void pushAttributeVertexToVector(TiXmlElement *xmlElement, Vector3f &targetVector);
   static std::string mandatoryAttributeMessage;
   static std::string invalidElementMessage;
-  static void throwMandatoryAttributeException(std::string message);
-  static void extractPositionAndRotation(TiXmlElement* spawnElement, Entity &entity);
+  static void throwMandatoryAttributeException(const std::string &message);
+  static void extractPositionAndRotation(TiXmlElement *spawnElement, Entity &entity);
 };
 
 } /* namespace glPortal */
