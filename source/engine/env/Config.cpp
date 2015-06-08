70dc1013dfc082af1aac5fb141c4ecfe476a8874
Julian Thijssen <nimthora@gmail.com>
2015-06-08 12:59:31 +0200

    Added exception handling to the config loading

--- source/engine/env/Config.cpp
+++ source/engine/env/Config.cpp
@@ -3,6 +3,8 @@
 #include "Environment.hpp"
 #include "ConfigFileParser.hpp"
 
+#include <iostream>
+
 namespace glPortal {
   const std::string Config::TRUE = "yes";
   const std::string Config::SENSITIVITY = "sensitivity";
@@ -24,26 +26,31 @@ namespace glPortal {
   std::string Config::map = "n1.xml";
 
   void Config::load() {
-    ConfigFileParser *config = Environment::getConfigPointer();
+    try {
+      ConfigFileParser *config = Environment::getConfigPointer();
   
-    Config::fullscreen = config->getStringByKey(Config::FULLSCREEN) == Config::TRUE;
-    Config::antialiasing = config->getStringByKey(Config::ANTIALIASING) == Config::TRUE;
-    Config::vsync = config->getStringByKey(Config::VSYNC) == Config::TRUE;
-    Config::sound = config->getStringByKey(Config::SOUND) == Config::TRUE;
-	Config::sensitivity = config->getFloatByKey(Config::SENSITIVITY);
+      Config::fullscreen = config->getStringByKey(Config::FULLSCREEN) == Config::TRUE;
+      Config::antialiasing = config->getStringByKey(Config::ANTIALIASING) == Config::TRUE;
+      Config::vsync = config->getStringByKey(Config::VSYNC) == Config::TRUE;
+      Config::sound = config->getStringByKey(Config::SOUND) == Config::TRUE;
+	  Config::sensitivity = config->getFloatByKey(Config::SENSITIVITY);
 
-    std::string width = config->getStringByKey(Config::WIDTH);
-    if (width == "auto") {
-      Config::width = -1;
-    } else {
-      Config::width = std::stoi(width);
-    }
+      std::string width = config->getStringByKey(Config::WIDTH);
+      if (width == "auto") {
+        Config::width = -1;
+      } else {
+        Config::width = std::stoi(width);
+      }
 
-    std::string height = config->getStringByKey(Config::HEIGHT);
-    if (height == "auto") {
-      Config::height = -1;
-    } else {
-      Config::height = std::stoi(height);
+      std::string height = config->getStringByKey(Config::HEIGHT);
+      if (height == "auto") {
+        Config::height = -1;
+      } else {
+        Config::height = std::stoi(height);
+      }
+    } catch (const std::invalid_argument &e) {
+      std::cout << "Failed to load configuration due to an invalid argument" << std::endl;
     }
   }
 } /* namespace glPortal */
+
