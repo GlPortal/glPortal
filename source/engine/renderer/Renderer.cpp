af6564b5a27171097b62c24c96c20bfd351f1f1b
Julian Thijssen <nimthora@gmail.com>
2015-07-08 20:06:35 +0200

    [fix] Rescaled text

--- source/engine/renderer/Renderer.cpp
+++ source/engine/renderer/Renderer.cpp
@@ -200,11 +200,11 @@ void Renderer::renderUI(const Camera &cam) {
   renderMesh(cam, unshaded, crosshairMtx, mesh, mat);
 
   // Title
-  setFont("Pacaya", 0.375f);
+  setFont("Pacaya", 1.5f);
   renderText(cam, "GlPortal", 25, vpHeight - 75);
 
   // FPS counter
-  setFont("Pacaya", 0.8f);
+  setFont("Pacaya", 0.5f);
   renderText(cam, std::string("FPS: ") + std::to_string(Game::fps.getFps()), 10, vpHeight - 25);
 }
 
