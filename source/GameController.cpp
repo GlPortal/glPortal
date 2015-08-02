0739c8acc9a46603e3509fef3b1c10c02d103a3d
Henry Hirsch <henry@w3-net.de>
2015-08-02 12:50:12 +0200

    [fix] Improvised game controller class.

---
+++ source/GameController.cpp
@@ -0,0 +1,65 @@
+#include "GameController.hpp"
+#include <SDL2/SDL_keyboard.h>
+#include <stdexcept>
+#include <string>
+#include <cstdio>
+#include <iostream>
+
+#include "engine/env/ConfigFileParser.hpp"
+#include "engine/env/Environment.hpp"
+#include "engine/env/ArgumentsParser.hpp"
+#include <engine/env/System.hpp>
+#include <engine/SoundManager.hpp>
+#include <engine/core/event/Observer.hpp>
+#include <engine/core/event/Dispatcher.hpp>
+#include <engine/core/event/observer/MusicObserver.hpp>
+#include "Input.hpp"
+#include "Game.hpp"
+
+namespace glPortal {
+
+GameController::GameController(Game *game) {
+  this->game = game;
+  this->world = game->getWorld();
+}
+
+void GameController::handleInput() {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    handleEvent(event);
+  }
+}
+
+void GameController::handleEvent(const SDL_Event &event) {
+  if (event.type == SDL_QUIT) {
+    game->close();
+  }
+  if (event.type == SDL_KEYDOWN) {
+    int key = event.key.keysym.scancode;
+    int mod = event.key.keysym.mod;
+
+    Input::keyPressed(key, mod);
+
+    if (key == SDL_SCANCODE_Q) {
+      game->close();
+    }
+  }
+  if (event.type == SDL_KEYUP) {
+    int key = event.key.keysym.scancode;
+    int mod = event.key.keysym.mod;
+
+    Input::keyReleased(key, mod);
+  }
+  if (event.type == SDL_MOUSEBUTTONDOWN) {
+    if (event.button.button == SDL_BUTTON_LEFT) {
+      world->shootPortal(1);
+    }
+    if (event.button.button == SDL_BUTTON_RIGHT) {
+      world->shootPortal(2);
+    }
+    if (event.button.button == SDL_BUTTON_MIDDLE  and Config::isHidePortalsByClick()) {
+      world->hidePortals();
+    }
+  }
+}
+}
---
+++ source/GameController.hpp
@@ -0,0 +1,22 @@
+#ifndef GAME_CONTROLLER_HPP
+#define GAME_CONTROLLER_HPP
+
+#include "Window.hpp"
+#include "World.hpp"
+#include <SDL2/SDL_events.h>
+
+namespace glPortal {
+class Game;
+class GameController {
+public:
+  GameController(Game *game);
+  void handleInput();
+  void handleEvent(const SDL_Event &event);
+private:
+  World *world;
+  Game *game;
+};
+
+} /* namespace glPortal */
+
+#endif /* GAME_CONTROLLER_HPP */
