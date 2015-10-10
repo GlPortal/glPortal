362b916b06deb9d86c27b90da091308ee89289cc
Henry Hirsch <henry@w3-net.de>
2015-10-10 12:28:22 +0200

    Moved most tools to seperate repository.

--- tools/blender/copyGameData.py
+++
@@ -1,7 +0,0 @@
-import os
-import shutil
-
-if os.path.exists(os.path.expanduser("~/.glportal/")):
-    shutil.rmtree(os.path.expanduser("~/.glportal/"))
-#os.makedirs(os.path.expanduser("~/.glportal/"))
-shutil.copytree("../../data", os.path.expanduser("~/.glportal/data"))
