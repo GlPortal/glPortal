f36c72f2eb76b77c1579e32d840cc955283d8d68
Henry Hirsch <henry@w3-net.de>
2015-12-09 20:39:48 +0100

    Add skeleton for terminal renderer

---
+++ source/engine/renderer/TerminalRenderer.cpp
@@ -0,0 +1,16 @@
+#include "TerminalRenderer.hpp"
+#include "Renderer.hpp"
+#include <assets/model/MeshLoader.hpp>
+#include <assets/texture/TextureLoader.hpp>
+#include <assets/text/FontLoader.hpp>
+#include <assets/shader/ShaderLoader.hpp>
+#include <assets/material/MaterialLoader.hpp>
+
+#include <cstdio>
+#include <vector>
+#include <string>
+
+namespace glPortal {
+void TerminalRenderer::render(Renderer &renderer) {
+}
+} /* namespace glPortal */
