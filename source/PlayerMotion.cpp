1078b774c288547db563868fd3a656ad697b50e1
Henry Hirsch <henry@w3-net.de>
2015-12-24 09:37:41 +0100

    Apply coding guidelines to player motion

--- source/PlayerMotion.cpp
+++ source/PlayerMotion.cpp
@@ -53,7 +53,8 @@ void PlayerMotion::move(double dtime) {
        movingBack    = Input::isKeyDown(SDL_SCANCODE_S) or Input::isKeyDown(SDL_SCANCODE_DOWN),
        strafingLeft  = Input::isKeyDown(SDL_SCANCODE_A) or Input::isKeyDown(SDL_SCANCODE_LEFT),
        strafingRight = Input::isKeyDown(SDL_SCANCODE_D) or Input::isKeyDown(SDL_SCANCODE_RIGHT),
-       jumping       = Input::isKeyDown(SDL_SCANCODE_SPACE) or Input::isKeyDown(SDL_SCANCODE_BACKSPACE);
+       jumping = Input::isKeyDown(SDL_SCANCODE_SPACE) or Input::isKeyDown(SDL_SCANCODE_BACKSPACE);
+
   float rot = headAngle.heading;
   Vector3f movement;
   KinematicCharacterController &ctrl = *entity.getComponent<Player>().controller;
