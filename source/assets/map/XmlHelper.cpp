36e5c4c39d30640d1989ca042319adc509510b3c
Henry Hirsch <henry@w3-net.de>
2015-12-26 17:05:29 +0100

    Apply code guidelines to xml helper

--- source/assets/map/XmlHelper.cpp
+++ source/assets/map/XmlHelper.cpp
@@ -10,13 +10,15 @@ using namespace std;
 namespace glPortal {
 
 std::string XmlHelper::mandatoryAttributeMessage("Mandatory attribute has not been defined.");
-std::string XmlHelper::invalidElementMessage("pushAttributeToVector received an invalid XML-Element.");
+std::string XmlHelper::invalidElementMessage(
+"pushAttributeToVector received an invalid XML-Element."
+                                             );
 /**
   * Pushes vector coordinates from an XML-element to a Vector3f.
   * Trying to pull attributes from a non-existing element is considered an exception.
   */
 void XmlHelper::pushAttributeVertexToVector(XMLElement *xmlElement, Vector3f &targetVector) {
-  if(xmlElement) {
+  if (xmlElement) {
     int xQueryResult = xmlElement->QueryFloatAttribute("x", &targetVector.x);
     int yQueryResult = xmlElement->QueryFloatAttribute("y", &targetVector.y);
     int zQueryResult = xmlElement->QueryFloatAttribute("z", &targetVector.z);
