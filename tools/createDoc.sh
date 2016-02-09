bf9e30adb0df963602e472090986154a22ad469c
Henry Hirsch <henry@w3-net.de>
2016-02-09 22:01:41 +0100

    Add markdown flavor conversion into doc creation

--- tools/createDoc.sh
+++ tools/createDoc.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 cd ..;
+rm -rf documentation/specificationCompiled/;
+mkdir documentation/specificationCompiled/;
+cp -r documentation/specification/* documentation/specificationCompiled/
+# Using https://github.com/hhirsch/markdown-gh-to-dox
+find documentation/specificationCompiled/ -exec gh2dox {} \;
 doxygen;
