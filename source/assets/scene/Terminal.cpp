837e8e9051a449d9ab6ff8539eabdbf289e8c39c
Dorian Wouters <gravgun@openmailbox.org>
2016-02-24 11:26:03 +0100

    Remove files moved to Radix repo

--- source/assets/scene/Terminal.cpp
+++
@@ -1,9 +0,0 @@
-#include "Terminal.hpp"
-
-namespace glPortal {
-
-Terminal::Terminal():
-  enabled(false),
-  textColor(0, 0, 0, 1), backgroundColor(1, 1, 1, 1) {
-}
-} /* namespace glPortal */
--- source/engine/core/diag/Logger.cpp
+++
@@ -1,9 +0,0 @@
-#include "Logger.hpp"
-
-namespace glPortal {
-
-const char* Logger::getName() const {
-  return "Empty logger";
-}
-
-} /* namespace glPortal */
