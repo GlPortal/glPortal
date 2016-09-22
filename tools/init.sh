1dfe10bdb5e41fbe75172986061d11d775bbab1f
Henry Hirsch <henry@w3-net.de>
2016-09-22 20:39:39 +0200

    Comment on function of init.sh

--- tools/init.sh
+++ tools/init.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Run this from the project root
+# it will prepare your submodules
+# and build radix and glportal
 git submodule update --init --recursive;
 cd external/RadixEngine;
 cmake ./; make;
