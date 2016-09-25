1ff5d9d9b00fb8d3793355b5c031e2fea69f7edf
Geert Custers <geert.aj.custers@gmail.com>
2016-09-25 18:06:07 +0200

    Switch to BaseGame system

--- source/Game.cpp
+++ source/Game.cpp
@@ -17,15 +17,12 @@
 #include <radix/core/diag/Throwables.hpp>
 #include <radix/system/PlayerSystem.hpp>
 #include <radix/system/PhysicsSystem.hpp>
-#include <util/sdl/Fps.hpp>
 #include "renderer/UiRenderer.hpp"
 #include <SDL2/SDL_keyboard.h>
 
 using namespace radix;
 
 namespace glPortal {
-
-Fps Game::fps;
 
 Game::Game() :
   world(window),
@@ -42,32 +39,6 @@ Game::Game() :
   }
 }
 
-void Game::init() {
-  if(config.cursorVisibility) {
-    window.unlockMouse();
-  } else {
-    window.lockMouse();
-  }
-  world.create();
-  renderer = std::make_unique<Renderer>(world);
-  camera = std::make_unique<Camera>();
-  { World::SystemTransaction st = world.systemTransact();
-    st.addSystem<PlayerSystem>();
-    st.addSystem<PhysicsSystem>();
-  }
-  nextUpdate = SDL_GetTicks(), lastUpdate = 0, lastRender = 0;
-
-  renderer->setViewport(&window);
-}
-
-bool Game::isRunning() {
-  return !closed;
-}
-
-World* Game::getWorld() {
-  return &world;
-}
-
 void Game::loadMap() {
   XmlMapLoader mapLoader(world);
   std::string mapPath = config.mapPath;
@@ -78,58 +49,16 @@ void Game::loadMap() {
   }
 }
 
-void Game::update() {
-  int skipped = 0;
-  currentTime = SDL_GetTicks();
-
-  while (currentTime > nextUpdate && skipped < MAX_SKIP) {
-    nextUpdate += SKIP_TIME;
-    skipped++;
-  }
-  int elapsedTime = currentTime - lastUpdate;
-  SoundManager::update(world.getPlayer());
-  world.update(TimeDelta::msec(elapsedTime));
-  lastUpdate = currentTime;
-}
-
-void Game::processInput() {
-  window.processEvents();
-  if (window.isKeyDown(SDL_SCANCODE_Q)) {
-    close();
-  }
-}
-
-void Game::cleanUp() {
-  world.destroy();
-  window.close();
-}
-
 void Game::render() {
   prepareCamera();
 
   renderer->render((currentTime-lastRender)/1000., *camera.get());
   radix::RenderContext renderContext(*renderer.get());
-  UiRenderer::render(renderContext, world);
+  UiRenderer::render(renderContext, (World)world);
 
   fps.countCycle();
   window.swapBuffers();
   lastRender = currentTime;
-}
-
-void Game::prepareCamera() {
-  camera->setPerspective();
-  int viewportWidth, viewportHeight;
-  window.getSize(&viewportWidth, &viewportHeight);
-  camera->setAspect((float)viewportWidth / viewportHeight);
-  const Transform &playerTransform = world.getPlayer().getComponent<Transform>();
-  Vector3f headOffset(0, playerTransform.getScale().y, 0);
-  camera->setPosition(playerTransform.getPosition() + headOffset);
-  const Player &playerComponent = world.getPlayer().getComponent<Player>();
-  camera->setOrientation(playerComponent.getHeadOrientation());
-}
-
-void Game::close() {
-  closed = true;
 }
 
 } /* namespace glPortal */
--- source/Game.hpp
+++ source/Game.hpp
@@ -4,9 +4,9 @@
 #include <radix/Window.hpp>
 #include <radix/renderer/Renderer.hpp>
 #include <radix/env/Config.hpp>
+#include <radix/BaseGame.hpp>
 
 #include "World.hpp"
-#include "util/sdl/Fps.hpp"
 
 namespace glPortal {
 
@@ -14,36 +14,12 @@ const int UPDATE_RATE = 60;
 const int SKIP_TIME = 1000 / UPDATE_RATE;
 const int MAX_SKIP = 5;
 
-class GameController;
-class Game {
+class Game : public radix::BaseGame {
 public:
-  static Fps fps;
-
   Game();
-  Game(Game&) = delete;
-  Game(Game&&) = delete;
-  bool isRunning();
-  void processInput();
-  void update();
-  void cleanUp();
-  void close();
   void render();
-  World* getWorld();
-  inline radix::Window& getWindow() {
-    return window;
-  }
-
 private:
-  unsigned int currentTime = 0, nextUpdate = 0, lastUpdate = 0, lastRender = 0;
-  radix::Window window;
-  World world;
-  std::unique_ptr<radix::Renderer> renderer;
-  std::unique_ptr<radix::Camera> camera;
-  bool closed;
-  void prepareCamera();
-  void init();
   void loadMap();
-  radix::Config &config;
 };
 
 } /* namespace glPortal */
