bb03fa430ce02a75b8818d37b90b7c8d5334d79c
Geert Custers <geert.aj.custers@gmail.com>
2016-09-30 16:58:32 +0200

    Fix includes in Game.cpp

--- source/Game.cpp
+++ source/Game.cpp
@@ -1,25 +1,16 @@
 #include "Game.hpp"
 
-#include <stdexcept>
-#include <string>
-#include <cstdio>
 #include <iostream>
 
-#include <SDL2/SDL_timer.h>
-
-#include <radix/component/Transform.hpp>
 #include <radix/component/Player.hpp>
 #include <radix/env/Environment.hpp>
 #include <radix/env/ArgumentsParser.hpp>
-#include <radix/env/Util.hpp>
 #include <radix/map/XmlMapLoader.hpp>
 #include <radix/SoundManager.hpp>
-#include <radix/core/diag/Throwables.hpp>
 #include <radix/system/PlayerSystem.hpp>
 #include <radix/system/PhysicsSystem.hpp>
-#include "renderer/UiRenderer.hpp"
+
 #include "GameState.hpp"
-#include <SDL2/SDL_keyboard.h>
 
 using namespace radix;
 
