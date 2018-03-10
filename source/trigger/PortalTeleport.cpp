d64c01744af9c79fe271e1764114ab16428b06a6
turlututututu <35852578+turlututututu@users.noreply.github.com>
2018-03-10 06:22:14 +0100

    Fix orientation after teleportation (#137)
    * Take the player orientation when computing the new orientation after passing through a portal
    
    * Add comments
    
    * Remove unused import
    
    * Convert names to camelCase
    
    Fixes #137

--- source/trigger/PortalTeleport.cpp
+++ source/trigger/PortalTeleport.cpp
@@ -21,11 +21,25 @@ void PortalTeleport::setAction(radix::Entity &trigger, const std::string &destin
   dynamic_cast<radix::entities::Trigger&>(trigger).setActionOnMove([&trigger, destination]
                                                              (radix::entities::Trigger &trigger) {
     radix::entities::Player &player = trigger.world.getPlayer();
-    if (trigger.world.destinations.find(destination)
-        != trigger.world.destinations.end()) {
+    if (trigger.world.destinations.find(destination) != trigger.world.destinations.end()) {
+      const radix::Quaternion &destinationOrientation = trigger.world.destinations.at(destination).orientation;
+      const radix::Quaternion &oldPlayerOrientation = player.getHeadOrientation();
+
+      /*
+       * destinationOrientation : describes the rotation needed to tranform a vector in the
+       * world coordinates to a vector following the orientation of the destination portal.
+       *
+       * oldPlayerOrientation : describes the rotation to transform a reference vector into a vector
+       * following the player orientation
+       *
+       * The product of two quaternions can be seen as the composition of two rotations.
+       * (all quaternions can be interpreted as a rotation)
+       */
+      radix::Quaternion newPlayerOrientation = destinationOrientation * oldPlayerOrientation;
+
       player.setPosition(trigger.world.destinations.at(destination).position);
-      player.setOrientation(trigger.world.destinations.at(destination).orientation);
-      player.setHeadOrientation(trigger.world.destinations.at(destination).orientation);
+      player.setOrientation(newPlayerOrientation);
+      player.setHeadOrientation(newPlayerOrientation);
     }
   });
 }
