ca2367cfee10d724ee83c71710ae9728f26d2fdc
Henry Hirsch <henry@w3-net.de>
2018-05-12 09:10:21 +0200

    Fix player orientation in teleport trigger

--- source/trigger/PortalTeleport.cpp
+++ source/trigger/PortalTeleport.cpp
@@ -23,19 +23,7 @@ void PortalTeleport::setAction(radix::Entity &trigger, const std::string &destin
     radix::entities::Player &player = trigger.world.getPlayer();
     if (trigger.world.destinations.find(destination) != trigger.world.destinations.end()) {
       const radix::Quaternion &destinationOrientation = trigger.world.destinations.at(destination).orientation;
-      const radix::Quaternion &oldPlayerOrientation = player.getHeadOrientation();
-
-      /*
-       * destinationOrientation : describes the rotation needed to tranform a vector in the
-       * world coordinates to a vector following the orientation of the destination portal.
-       *
-       * oldPlayerOrientation : describes the rotation to transform a reference vector into a vector
-       * following the player orientation
-       *
-       * The product of two quaternions can be seen as the composition of two rotations.
-       * (all quaternions can be interpreted as a rotation)
-       */
-      radix::Quaternion newPlayerOrientation = destinationOrientation * oldPlayerOrientation;
+      radix::Quaternion newPlayerOrientation = destinationOrientation;
 
       player.setPosition(trigger.world.destinations.at(destination).position);
       player.setOrientation(newPlayerOrientation);
